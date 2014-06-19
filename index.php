ff2847e9c4cf6f5274eabd8955548781f352006f
HerrBaron <marco.schmitz@gmail.com>
2014-06-19 21:59:08 +0200

    Modal Login Dialog

--- index.php
+++ index.php
@@ -18,12 +18,51 @@
             <div class="jumbotron">
                 <h1>Welcome to spieltagtipp.de</h1>
                 <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-                <p><a class="btn btn-primary btn-lg" onClick='window.location.href = "login.php" '>Login</a> 
+                <p><button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#LoginModal">Login</button> 
                     <a class="btn btn-primary btn-lg" onClick='window.location.href = "register.php" '>Register</a></p>
-                    <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Launch demo modal</button>
             </div>
         </div><!-- /.container -->
-        
+        <!-- Modal -->
+        <div class="modal fade" id="LoginModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <form class="form-horizontal" action="UserApplication.php" method="POST">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title" id="myModalLabel">Login</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group<?= ($validator->getError("useremail") != "") ? " has-error" : "" ?>">
+                        <label for="Name" class="col-lg-2 control-label" >
+                            Email<?= ($validator->getError("useremail") != "") ? " - " . $validator->getError("useremail") : "" ?>
+                        </label>
+                        <div class="col-lg-10">
+                            <input type="text" class="form-control" id="useremail" name="useremail" placeholder="useremail" value="<?= $validator->getValue("username") ?>">
+                        </div>
+                    </div>
+                    <div class="form-group<?= ($validator->getError("password") != "") ? " has-error" : "" ?>">
+                        <label for="inputPassword" class="col-lg-2 control-label">
+                            Password<?= ($validator->getError("password") != "") ? " - " . $validator->getError("password") : "" ?>
+                        </label>
+                        <div class="col-lg-10">
+                            <input type="password" class="form-control" id="inputPassword" name="password" placeholder="Password">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" checked id="rememberme" name="rememberme"> Remember Me
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="hidden" name="login" value="1">
+                        <button class="btn btn-default">Cancel</button>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>
         
 <?php   
     include_once 'template/include_footer.php';
